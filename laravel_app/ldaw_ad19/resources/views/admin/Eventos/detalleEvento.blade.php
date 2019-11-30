3b24ac0c96013b6abf00fc0e68987fcc59e79db8
Marla <marla@brounie.com>
2019-11-30 15:42:54 -0600

    consultar asistentes

---
+++ laravel_app/ldaw_ad19/resources/views/admin/Eventos/detalleEvento.blade.php
@@ -0,0 +1,109 @@
+@extends('layouts.app')
+@include('includes.head')
+@section('content')    
+    
+    <div class = "container">
+        <div class="row justify-content-center">
+            <!-- sidebar content -->
+            <div id="sidebar" class="col-md-3 mb-4">   
+                @include('includes.sidebar')       
+            </div>
+            <br>       
+            <div class="col-md-9 mb-4">
+                <div class="row row-cols-1 row-cols-md-2">                 
+                    @foreach($evento as $item)
+                        <h2 class="mb-8"><b>{{$item->titulo_evento}}</b></h2> 
+                        <div class="row mx-md-n5">
+                            <div class="col px-md-5">
+                                <div class="p-3  mb-3">
+                                    <div class="row mb-2">
+                                        <div class="col-md-2">
+                                           <i class="fas fa-calendar-day "></i>
+                                        </div>
+                                        <div class="col-md-8">
+                                            <h6>Fecha: {{$item->fecha}}</h6>
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-2">
+                                        <div class="col-md-2">
+                                        <i class="fas fa-clock"></i>
+                                        </div>
+                                        <div class="col-md-8">
+                                            <h6>Hora: {{$item->hora}}</h6>
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-2">
+                                        <div class="col-md-2">
+                                            <i class="fas fa-map-marker-alt"></i>
+                                        </div>
+                                        <div class="col-md-8">
+                                            <h6>Sitio: {{$item->sitio}}, {{$item->ciudad}}</h6>
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-2">
+                                        <div class="col-md-2">
+                                            <i class="fas fa-user-check"></i>
+                                        </div>
+                                        <div class="col-md-8">
+                                            <h6>Lugares Diponibles: {{$item->capacidad_maxima - $item->afluencia_actual}}</h6>
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-2">
+                                        <div class="col-md-2">
+                                            <i class="fas fa-university"></i>
+                                        </div>
+                                        <div class="col-md-8">
+                                            <h6>Institución: {{$item->nombre}}</h6>
+                                        </div>
+                                    </div>
+
+                                    <h4 class="mb-5 justify-content-center">{{$item->descripcion}}</h4>
+                                </div>
+                            </div>
+                            <div class="col px-md-">
+                                <div class="p-3 ">
+                                    <img src="img/eventos.jpg" class="rounded  img-fluid " alt="...">
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach                          
+                </div>
+                <div>
+                    <p>
+                        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                            Asistentes
+                        </button>
+                    </p>
+                    <div class="collapse" id="collapseExample">
+                        <div class="card card-body">
+                        <table class="table">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col">Apellidos</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($asistentes as $asistente)
+                            <tr>
+                                <th scope="row">{{$asistente->name}}</th>
+                                <th scope="row">{{$asistente->apellido_paterno." ".$asistente->apellido_materno}}</th>
+                            </tr>
+                            @endforeach 
+                            </tbody>
+                        </table>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+        </div>
+    </div>
+
+<footer class="row">
+    @include('includes.footer')
+</footer>
+@endsection
--- laravel_app/ldaw_ad19/resources/views/components/card.blade.php
+++ laravel_app/ldaw_ad19/resources/views/components/card.blade.php
@@ -1,12 +1,14 @@
 
 <div class="col-md-4 mb-4">
-    <div class="card h-100 w-400">
-        <img src="img/eventos.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title"><strong>{{$item->titulo_evento}}</strong></h5>
-            <p class="card-text">{{$item->descripcion}}</p>
+    <a href="{!! route('evento', ['id' => $item->id]) !!}">
+        <div class="card h-100 w-400">
+            <img src="img/eventos.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title" style="color:black;"><strong>{{$item->titulo_evento}}</strong></h5>
+                <p class="card-text" style="color:black;">{{$item->descripcion}}</p>
+            </div>
         </div>
-    </div>
+    </a>
 </div>
 
 
